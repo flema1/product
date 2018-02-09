f5f7e2b7ab707bb52dfb148c71bee106dbe315ff
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-02-09 16:54:09 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+const app = express();
+require('dotenv').config();
+app.use(logger('dev'));
+
+// middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+// static files
+app.use(express.static('public'));
+// static files, serving react build
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/* setting up port & listen */
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT} 00`);
+});
+
+app.get('/', (req, res) => {
+  res.send('We are live!');
+});
+
+app.use('/', index);
+app.use('/users', users);
+
+app.get('*', (req, res) => {
+    const err = new Error('not found!');
+    res.status(404).send(err);
+});
