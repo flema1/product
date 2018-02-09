bdc55ed87784aec2000a71b7b6e4eb84941ff9e3
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-02-09 18:49:00 -0500

    add initial product seeding

---
+++ models/product.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Product = sequelize.define('Product', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    title: DataTypes.STRING,
+    category: DataTypes.STRING,
+    price: DataTypes.DECIMAL(5, 2),
+    rating: DataTypes.DECIMAL(2, 1),
+    stock: DataTypes.INTEGER, 
+  }, {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: 'product'
+  }, {
+  });
+
+  return Product;
+};
