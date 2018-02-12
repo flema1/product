2408188d8cd1f7e20396a60dfff8f6645fb15731
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-02-12 02:15:54 -0500

    add home component

---
+++ client/src/containers/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Homepage extends Component {
+  componentDidMount() {
+    // document.body.scrollTop = 0;
+    // document.querySelector('.menu').classList.remove('open');
+  }
+  render() {
+    return (
+      <main className="main">
+          Home
+      </main>
+    );
+  }
+}
+
+export default Homepage;
